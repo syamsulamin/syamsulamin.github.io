582000aead7dffecba2677df0017780aa5b91212
syamsulamin <umaminzamin@gmail.com>
2021-04-10 16:32:56 +0700

    deploy 2

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:syamsulamin/syamsulamin.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:syamsulamin/syamsulamin.github.io.git main:gh-pages
+# git push -f git@github.com:syamsulamin/syamsulamin.github.io.git main:gh-pages
 
 cd -
