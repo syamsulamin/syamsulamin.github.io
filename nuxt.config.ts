a4e0fe00d9a6dd259bc5e76a1a11dea0bec6ded8
cybanjar air <syamsulamin@syamsuls-MacBook-Air.local>
2025-11-26 22:47:32 +0700

    feat: integrate portfolio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  app: {
-    baseURL: '/syamsulamin.github.io/',
-    buildAssetsDir: 'assets',
-  },
   modules: [
-    '@nuxthq/ui'
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+    'motion-v/nuxt'
   ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2024-11-01',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
 })
